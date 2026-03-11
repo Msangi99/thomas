b209d075c24ddb032054baa6d12a49595b8b07c8
Msangi99 <doniaparoma99@gmail.com>
2026-03-12 02:31:57 +0300

    sss

--- config/tra.php
+++ config/tra.php
@@ -1,13 +1,11 @@
 <?php
 
 return [
-    // Set to false to skip TRA fiscalization (e.g. when certificate is not yet configured)
     'enabled' => env('TRA_ENABLED', true),
-
     'env' => env('TRA_ENV', 'production'), // test or production
     'tin' => env('TRA_TIN', '156692506'),
-    'cert_path' => env('TRA_CERT_PATH', storage_path('app/tra/certificate.pfx')),
-    'password' => env('TRA_PASSWORD', 'HD8v#4tN'), // PFX certificate password (from TRA)
+    'cert_path' => env('TRA_CERT_PATH', storage_path('app/tra/10TZ101424.pfx')),
+    'password' => env('TRA_PASSWORD', 'HD8v#4tN'), // PFX Password and Initial Registration Password
     'cert_serial' => env('TRA_CERT_SERIAL', '10TZ101424'), // e.g. 10TZ...
     'base_url' => [
         'test' => 'https://virtual.tra.go.tz/efdmsRctApi/api',
